63347790ee925901c2cbf3c25ff47c475f00348c
João Pedro S. <joaopedrim17@gmail.com>
2026-03-27 19:18:58 -0300

    Add files via upload

---
+++ 3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] vetor = new int[8];
+        int numero;
+        bool encontrado = false;
+
+        for (int i = 0; i < 8; i++)
+        {
+            Console.Write("Digite um número: ");
+            vetor[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.Write("\nDigite um número para procurar: ");
+        numero = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < 8; i++)
+        {
+            if (vetor[i] == numero)
+            {
+                Console.WriteLine("Número encontrado na posição: " + i);
+                encontrado = true;
+            }
+        }
+
+        if (!encontrado)
+        {
+            Console.WriteLine("O número não se encontra no vetor");
+        }
+    }
+}
---
+++ 4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] vetor1 = new int[10];
+        int[] vetor2 = new int[10];
+        int[] vetor3 = new int[10];
+
+        Console.WriteLine("Digite os valores do primeiro vetor:");
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("Número: ");
+            vetor1[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("\nDigite os valores do segundo vetor:");
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("Número: ");
+            vetor2[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            vetor3[i] = vetor1[i] + vetor2[i];
+        }
+
+        Console.WriteLine("\nVetor resultante:");
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(vetor3[i]);
+        }
+    }
+}
